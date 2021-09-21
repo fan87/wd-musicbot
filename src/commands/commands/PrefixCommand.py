e12d503c5f13aac046a3038395d32944240e2ce6
fan87 <fan87.TW@gmail.com>
2021-09-21 18:52:59 +0800

    Permission

--- src/commands/commands/PrefixCommand.py
+++ src/commands/commands/PrefixCommand.py
@@ -18,17 +18,22 @@ class PrefixCommand(WDCommand):
 
 @main_command("設定指令前綴", PrefixCommand, prefix = "前綴 | DEFAULT")
 async def on_command(message: Message, prefix: str) -> None:
-    bot = InstanceManager.mainInstance
-    if prefix.upper() == "DEFAULT":
-        bot.data.get_guild(typing.cast(discord.Guild, message.guild)).prefix = "__ DEFAULT __"
+    member: discord.Member = message.author
+    permissions: discord.Permissions = member.guild_permissions
+    if permissions.administrator:
+        bot = InstanceManager.mainInstance
+        if prefix.upper() == "DEFAULT":
+            bot.data.get_guild(typing.cast(discord.Guild, message.guild)).prefix = "__ DEFAULT __"
+            bot.configsManager.save_data()
+            await wdutils.MessageUtil.reply_fancy_message(":white_check_mark: 成功設置指令前綴回預設值: `" + bot.commandsManager.get_prefix(message.guild) + "`", discord.Colour.green(),
+                                                          message)
+            return
+
+        bot.data.get_guild(typing.cast(discord.Guild, message.guild)).prefix = prefix
         bot.configsManager.save_data()
-        await wdutils.MessageUtil.reply_fancy_message(":white_check_mark: 成功設置指令前綴回預設值: `" + bot.commandsManager.get_prefix(message.guild) + "`", discord.Colour.green(),
+
+        await wdutils.MessageUtil.reply_fancy_message(":white_check_mark: 成功設置指令前綴至: `" + prefix + "`", discord.Colour.green(),
                                                       message)
-        return
 
-    bot.data.get_guild(typing.cast(discord.Guild, message.guild)).prefix = prefix
-    bot.configsManager.save_data()
-
-    await wdutils.MessageUtil.reply_fancy_message(":white_check_mark: 成功設置指令前綴至: `" + prefix + "`", discord.Colour.green(),
-                                                  message)
-
+    else:
+        await wdutils.MessageUtil.reply_fancy_message(":x: 只有管理員才能這樣做!", discord.Colour.red(), message)
